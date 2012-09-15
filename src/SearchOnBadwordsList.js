f9d6e3c9b38b3d3365cfdf9a9f314aa82d3da627
Helder <he7d3r@gmail.com>
2012-09-15 00:37:54 +0000

    Add a closure, enable strict mode and validate with JSLint

--- src/SearchOnBadwordsList.js
+++ src/SearchOnBadwordsList.js
@@ -2,21 +2,26 @@
  * Find which regular expression from badwords list detects the given expression
  * 
  * @source: based on function processBadWords from [[w:pt:MediaWiki:Gadget-antivandaltool.js]]
+ * @author: [[User:Helder.wiki]]
+ * @tracking: [[Special:GlobalUsage/User:Helder.wiki/Tools/SearchOnBadwordsList.js]] ([[File:User:Helder.wiki/Tools/SearchOnBadwordsList.js]])
  */
+/*jslint browser: true, white: true, plusplus: true, regexp: true, devel: true, continue: true */
+/*global jQuery, mediaWiki */
+( function ( mw, $ ) {
+'use strict';
 
 var findRegexMatching = function ( text ) {
-	var	data=$('#wpTextbox1').val().split('\n'),
-		phrase=[],
-		string=[],
-		regex;
-	var reg = {
-		//             (  (                                    )       )
-		repeatedChar: '(?:([^\\-\\|\\{\\}\\].\\s\'=wI:*#0-9a-f])\\2{2,})',
-		strings:'(?:<LIST>)',
-		phrases:'(?:^|[^/\\w])(?:<LIST>)(?![/\\w])'
-	};
-	for (var i=0; i<data.length; ++i) {
-		var s=data[i];
+	var	i, s, isPhrase, r,
+		data=$('#wpTextbox1').val().split('\n'),
+		regex,
+		reg = {
+			//             (  (                                    )       )
+			repeatedChar: '(?:([^\\-\\|\\{\\}\\].\\s\'=wI:*#0-9a-f])\\2{2,})',
+			strings:'(?:<LIST>)',
+			phrases:'(?:^|[^/\\w])(?:<LIST>)(?![/\\w])'
+		};
+	for (i=0; i<data.length; ++i) {
+		s=data[i];
 
 		// ignore empty lines, whitespace-only lines and lines starting with '<'
 		if (/^\s*$|^</.test(s)) { continue; }
@@ -24,7 +29,7 @@ var findRegexMatching = function ( text ) {
 		// lines beginning and ending with a (back-)slash (and possibly trailing
 		// whitespace) are treated as regexps
 		if (/^([\\\/]).*\1\s*$/.test(s)) {
-			var isPhrase=(s.charAt(0)=='/');
+			isPhrase=(s.charAt(0)==='/');
 			// remove slashes and trailing whitespace
 			s=s.replace(/^[\\\/]|[\\\/]\s*$/g, '');
 			// escape opening parens: ( -> (?:
@@ -32,7 +37,7 @@ var findRegexMatching = function ( text ) {
 			// http://rubular.com/r/8eqSxXIiVZ
 			s=s.replace(/\((?!\?)/g, '(?:');
 			// check that s represents a valid regexp
-			try { var r=new RegExp(s); }
+			try { r=new RegExp(s); }
 			catch (err) {
 				mw.notify(
 					'Aviso: a linha '+i+' da lista de palavrões foi ignorada' +
@@ -53,11 +58,10 @@ var findRegexMatching = function ( text ) {
 	regex = new RegExp( '(' + reg.repeatedChar + ')', 'gi');
 	if( regex.test( text ) ){
 		return 'A regex<br><code>' + regex + '</code><br>definida no código-fonte do script detectou a expressão "<code>' + mw.html.escape(text) + '</code>".';
-	} else {
-		return 'A expressão "<code>' + mw.html.escape(text) + '</code>" não foi detectada.';
 	}
-};
-var addLink = function(){
+	return 'A expressão "<code>' + mw.html.escape(text) + '</code>" não foi detectada.';
+},
+addLink = function(){
 	$( mw.util.addPortletLink(
 		'p-cactions',
 		'#',
@@ -76,3 +80,5 @@ var addLink = function(){
 if( mw.config.get('wgPageName') === 'Wikipédia:Software/Anti-vandal_tool/badwords' && $.inArray(mw.config.get('wgAction'), [ 'edit', 'submit' ]) !== -1 ){
 	$(addLink);
 }
+
+}( mediaWiki, jQuery ) );
